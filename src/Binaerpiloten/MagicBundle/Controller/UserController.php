fb7d3bda6f9e019ea8f633fb5cc2673a8b73f750
Andre Löffler <git@andre-loeffler.net>
2014-08-07 19:42:16 +0200

    change user: promote and demote, change user template and security layer

--- src/Binaerpiloten/MagicBundle/Controller/UserController.php
+++ src/Binaerpiloten/MagicBundle/Controller/UserController.php
@@ -57,4 +57,66 @@ class UserController extends Controller
     			'user'      => $entity,
     	);
     }
+    
+    /**
+     * Promotes User entity by name.
+     *
+     * @Route("/promote/{name}", name="user_promote")
+     * @Method("GET")
+     * @Template("FOSUserBundle:Profile:show.html.twig")
+     */
+    public function promoteAction($name)
+    {
+    	// Get our userManager, you must implement `ContainerAwareInterface`
+        $userManager = $this->container->get('fos_user.user_manager');
+
+        // Create our user and set details
+        $user = $userManager->findUserByUsername($name);
+        $roles = $user->getRoles();
+        // superadmin?
+        if (in_array('ROLE_ADMIN', $roles, true)) {
+	        $user->addRole('ROLE_SUPERADMIN');
+        }
+        // superadmin?
+        else if (in_array('ROLE_USER', $roles, true)) {
+        	$user->addRole('ROLE_ADMIN');
+        }
+
+        // Update the user
+        $userManager->updateUser($user, true);
+    
+    	return $this->redirect($this->generateUrl('user_show', array('id' => $user->getId())));
+    }
+    
+    /**
+     * Demotes User entity by name.
+     *
+     * @Route("/demote/{name}", name="user_demote")
+     * @Method("GET")
+     * @Template("FOSUserBundle:Profile:show.html.twig")
+     */
+    public function demoteAction($name)
+    {
+    	// Get our userManager, you must implement `ContainerAwareInterface`
+    	$userManager = $this->container->get('fos_user.user_manager');
+    
+    	// Create our user and set details
+    	$user = $userManager->findUserByUsername($name);
+    	$roles = $user->getRoles();
+    	// superadmin?
+    	if (in_array('ROLE_SUPERADMIN', $roles, true)) {
+    		if ($user == $this->getUser()) {
+    			$user->removeRole('ROLE_SUPERADMIN');
+    		}
+    	}
+    	// superadmin?
+    	else if (in_array('ROLE_ADMIN', $roles, true)) {
+    		$user->removeRole('ROLE_ADMIN');
+    	}
+    
+    	// Update the user
+    	$userManager->updateUser($user, true);
+    
+    	return $this->redirect($this->generateUrl('user_show', array('id' => $user->getId())));
+    }
 }
