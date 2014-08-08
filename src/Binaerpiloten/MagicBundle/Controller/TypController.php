bd0b2e2ea19cf59795857edbb2642c28c925ae8b
Andre Löffler <git@andre-loeffler.net>
2014-08-08 02:13:56 +0200

    unify crud templates step 2

--- src/Binaerpiloten/MagicBundle/Controller/TypController.php
+++ src/Binaerpiloten/MagicBundle/Controller/TypController.php
@@ -193,7 +193,7 @@ class TypController extends Controller
         if ($editForm->isValid()) {
             $em->flush();
 
-            return $this->redirect($this->generateUrl('typ_edit', array('id' => $id)));
+            return $this->redirect($this->generateUrl('typ'));
         }
 
         return array(
